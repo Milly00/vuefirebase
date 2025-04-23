ade56484898f67441bd2479044c7cda287932e80
Milly00 <83789940+Milly00@users.noreply.github.com>
2025-04-23 17:22:04 -0500

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,9 @@
 export const env_local = {
-  apiKey: "AIzaSyDC8c2eF-Kdrv_TRZvqfhPcZfOMVcS6fSU",
-  authDomain: "vue-app-694d8.firebaseapp.com",
-  projectId: "vue-app-694d8",
-  storageBucket: "vue-app-694d8.firebasestorage.app",
-  messagingSenderId: "220234191215",
-  appId: "1:220234191215:web:99b26365b0eca1a9e1989a",
-  measurementId: "G-VCY6SY74EJ",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
